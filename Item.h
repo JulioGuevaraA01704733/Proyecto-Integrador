d1d672e70a3d5314431b405d3d248fc5bd096caf
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2022-05-27 23:43:40 -0500

    Create Item.h

---
+++ Item.h
@@ -0,0 +1,85 @@
+#ifndef ITEM_H_
+#define ITEM_H_
+
+#include <string>
+
+#include <sstream>
+
+using namespace std;
+
+class Item {
+protected:
+  string nombre;
+  double valor;
+  string tipo_item;
+
+public:
+  Item(): nombre(""), valor(0), tipo_item("") {};
+Item(string nom, double val, string tipi):
+  nombre(nom), valor(val),tipo_item(tipi) {};
+
+  string get_nombre() {
+    return nombre;
+  }
+  double get_valor() {
+    return valor;
+  }
+  string get_tipo_item() {
+    return tipo_item;
+  }
+};
+
+class Empleado: public Item  {
+protected:
+  double salario_mensual;
+  string tipo_empleado;
+public:
+  Empleado(): Item("", 0,"Empleado"){};
+  Empleado(string nombre, double valor, double salar, string tipo_e): Item(nombre, valor, "Empleado"),
+  salario_mensual(salar), tipo_empleado(tipo_e){};
+  string get_nombre() {
+    return nombre;
+  }
+  double get_valor() {
+    return valor;
+  }
+  double pago_mensual() {
+    return valor*1;
+  }
+};
+
+class Jugador: public Empleado  {
+private:
+  string posicion;
+public:
+  Jugador(): Empleado("", 0,"Empleado",0,"Jugador") {};
+  Empleado(string nombre, double valor, double salar, string tipo_e, string pos): Empleado(nombre, valor, "Empleado", salario_mensual, "Jugador"),
+  posicion(pos){};
+  string get_nombre() {
+    return nombre;
+  }
+  double get_valor() {
+    return valor;
+  }
+  double pago_mensual() {
+    return salario_mensual*1.5;
+  }
+};
+class Entrenador: public Empleado  {
+private:
+  string especialidad;
+public:
+  Entrenador(): Empleado("",0,"Empleado",0,"Entrenador"){};
+  Empleado(string nombre, double valor, double salar, string tipo_e, string esp): Empleado(nombre, valor, "Empleado", salario_mensual, "Entrenador"),
+  especialidad(esp){};
+  string get_nombre() {
+    return nombre;
+  }
+  double get_valor() {
+    return valor;
+  }
+  double pago_mensual() {
+    return salario_mensual*1.25;
+  }
+};
+#endif
