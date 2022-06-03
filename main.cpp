846150f19a0af04d9370408bf8567ef0cecfa99c
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2022-06-03 17:45:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,4 +2,14 @@
 #include "Item.h"
 using namespace std;
 int main(){
-  
+  Club club;
+  club.crea_ejemplos();
+  club.muestra_items();
+  cout<<"test \n\n";
+  club.muestra_items("Inmueble");
+  club.agrega_empleado("Neymar", 150000.2);
+  club.muestra_items("Empleado");
+  club.agrega_inmueble("Complejo oficinas 2", "Calle Alfredo Talavera 455", 460.5);
+  club.muestra_items("Inmueble");
+  cout << "test \n\n";
+}
