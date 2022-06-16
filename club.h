4b8a7e22a352375702d53c77f83d182213483672
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2022-06-16 14:10:59 -0500

    Update club.h

--- club.h
+++ club.h
@@ -7,20 +7,20 @@
 
 #include <sstream>
 
-#include "item.h" //biblioteca que declara los objetos a usar
+#include "activo.h" //biblioteca que declara los objetos a usar
 
 using namespace std;
 
 const int MAX = 1000; //una constante que define el número máximo de elementos en los arreglos a utilizar
 class Club { 
   private:
-  Item * it[MAX]; //es apuntador para que sea dinámico y usar polimorfismo
+  Activo * it[MAX]; //es apuntador para que sea dinámico y usar polimorfismo
   int infraestructura;
   //declaramos constructor y otros métodos
   public:
   Club(): infraestructura(0) {}; //constructor por default
-  void muestra_items();
-  void muestra_items(string tipo_item);
+  void muestra_activos();
+  void muestra_activos(string tipo_activo);
   //se crea una funcion de agregar para cada tipo de empleado
   void agrega_jugador(string nombre, double salario_mensual);
   void agrega_entrenador(string nombre, double salario_mensual);
@@ -29,16 +29,16 @@ class Club {
   void agrega_inmueble(string nombre, string localizacion, double costo_mensual_mantenimiento);
   void agrega_patrocinio(string nombre, double valor_anual);
 };
-void Club::muestra_items() {
+void Club::muestra_activos() {
   //Ciclo que va imprimiendo cada elemento del arreglo
   for (int i = 0; i < infraestructura; i++)
     cout << it[i] -> to_string();
 }
-void Club::muestra_items(string tipo_item) {
+void Club::muestra_activos(string tipo_activo) {
 
   //Ciclo que va imprimiendo cada elemento del arreglo
   for (int i = 0; i < infraestructura; i++) {
-    if (it[i] -> get_tipo_item() == tipo_item)
+    if (it[i] -> get_tipo_activo() == tipo_activo)
       cout << it[i] -> to_string();
   }
 }
