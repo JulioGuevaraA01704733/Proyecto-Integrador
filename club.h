9d73266c5a56107bed4da75af7f6228e936fb904
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2022-06-16 16:33:42 -0500

    Update club.h

--- club.h
+++ club.h
@@ -14,7 +14,7 @@ using namespace std;
 const int MAX = 1000; //una constante que define el número máximo de elementos en los arreglos a utilizar
 class Club { 
   private:
-  Activo * it[MAX]; //es apuntador para que sea dinámico y usar polimorfismo
+  Activo * act[MAX]; //es apuntador para que sea dinámico y usar polimorfismo
   int infraestructura;
   //declaramos constructor y otros métodos
   public:
@@ -33,42 +33,42 @@ class Club {
 void Club::muestra_activos() {
   //Ciclo que va imprimiendo cada elemento del arreglo
   for (int i = 0; i < infraestructura; i++)
-    cout << it[i] -> to_string();
+    cout << act[i] -> to_string();
 }
 void Club::muestra_activos(string tipo_activo) {
 
   //Ciclo que va imprimiendo cada elemento del arreglo, con la condicion de que el tipo de activo sea el que le especificamos
   for (int i = 0; i < infraestructura; i++) {
-    if (it[i] -> get_tipo_activo() == tipo_activo)
-      cout << it[i] -> to_string();
+    if (act[i] -> get_tipo_activo() == tipo_activo)
+      cout << act[i] -> to_string();
   }
 }
 //se crea una funcion de agregar para cada tipo de empleado
 void Club::agrega_jugador(string nombre, double salario_mensual) {
-  it[infraestructura] = new Jugador(infraestructura, nombre, salario_mensual);
+  act[infraestructura] = new Jugador(infraestructura, nombre, salario_mensual);
   infraestructura++;
 }
 void Club::agrega_entrenador(string nombre, double salario_mensual) {
-  it[infraestructura] = new Entrenador(infraestructura, nombre, salario_mensual);
+  act[infraestructura] = new Entrenador(infraestructura, nombre, salario_mensual);
   infraestructura++;
 }
 void Club::agrega_oficinista(string nombre, double salario_mensual) {
-  it[infraestructura] = new Oficinista(infraestructura, nombre, salario_mensual);
+  act[infraestructura] = new Oficinista(infraestructura, nombre, salario_mensual);
   infraestructura++;
 }
 void Club::agrega_ejecutivo(string nombre, double salario_mensual) {
-  it[infraestructura] = new Ejecutivo(infraestructura, nombre, salario_mensual);
+  act[infraestructura] = new Ejecutivo(infraestructura, nombre, salario_mensual);
   infraestructura++;
 }
 void Club::agrega_inmueble(string nombre, string localizacion, double costo_mensual_mantenimiento) {
 
-  it[infraestructura] = new Inmueble(infraestructura, nombre, localizacion, costo_mensual_mantenimiento);
+  act[infraestructura] = new Inmueble(infraestructura, nombre, localizacion, costo_mensual_mantenimiento);
   infraestructura++;
 }
 void Club::agrega_patrocinio(string nombre, double valor_anual) {
 
   //new crea el objeto en tiempo de ejecución para usar polimorfismo
-  it[infraestructura] = new Patrocinio(infraestructura, nombre, valor_anual);
+  act[infraestructura] = new Patrocinio(infraestructura, nombre, valor_anual);
   infraestructura++;
 }
 double Club::calcula_balance_anual() {
@@ -77,16 +77,16 @@ double Club::calcula_balance_anual() {
   Inmueble *ip = NULL;
   Patrocinio *pp = NULL;              //No apuntan a nada, hasta que les damos valor dentro de la estructura condicional
   for (int i = 0; i < infraestructura; i++) {
-    if (it[i] -> get_tipo_activo() == "Empleado") {       //creamos un caso para cada clase hija de la clase activo
-      ep = (Empleado*) it[i];                            //volvemos el apuntador de la clase padre a un apuntador de la clase hija
+    if (act[i] -> get_tipo_activo() == "Empleado") {       //creamos un caso para cada clase hija de la clase activo
+      ep = (Empleado*) act[i];                            //volvemos el apuntador de la clase padre a un apuntador de la clase hija
       balance -= ep -> pago_anual_con_bonus();
     }
-    else if (it[i] -> get_tipo_activo() == "Inmueble"){
-      ip = (Inmueble*) it[i];
+    else if (act[i] -> get_tipo_activo() == "Inmueble"){
+      ip = (Inmueble*) act[i];
       balance -= ip -> get_costo_mensual_mantenimiento()*12;
     }
-    else if (it[i] -> get_tipo_activo() == "Patrocinio"){
-      pp = (Patrocinio*) it[i];
+    else if (act[i] -> get_tipo_activo() == "Patrocinio"){
+      pp = (Patrocinio*) act[i];
       balance += pp -> get_valor_anual();
     }
   }
