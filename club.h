dd3fc357d36657ec49d5cbbc4ecf5892583d63c7
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2022-06-16 21:21:15 -0500

    Update club.h

--- club.h
+++ club.h
@@ -16,35 +16,33 @@ class Club {
   private:
   Activo * act[MAX]; //es apuntador para que sea dinámico y usar polimorfismo
   int infraestructura;
-  //declaramos constructor y otros métodos
+  //declaramos constructor y demás métodos como públicos
   public:
   Club(): infraestructura(0) {}; //constructor por default
-  void muestra_activos();
-  void muestra_activos(string tipo_activo);
-  //se crea una funcion de agregar para cada tipo de empleado
+  void muestra_activos(); //muestra todos los activos
+  void muestra_activos(string tipo_activo); //muestra los activos del tipo que le especifiques
+  //se crea una funcion de agregar para cada tipo de empleado, otra para inmueble y otra para patrocinio 
   void agrega_jugador(string nombre, double salario_mensual);
   void agrega_entrenador(string nombre, double salario_mensual);
   void agrega_oficinista(string nombre, double salario_mensual);
   void agrega_ejecutivo(string nombre, double salario_mensual);
   void agrega_inmueble(string nombre, string localizacion, double costo_mensual_mantenimiento);
   void agrega_patrocinio(string nombre, double valor_anual);
-  double calcula_balance_anual();
+  double calcula_balance_anual(); //calcula las ganancias menos los gastos que se general anualmente con base en estos tres elementos (empleados, inmuebles patrocinios)
 };
 void Club::muestra_activos() {
-  //Ciclo que va imprimiendo cada elemento del arreglo
-  for (int i = 0; i < infraestructura; i++)
+  for (int i = 0; i < infraestructura; i++) //Ciclo que va imprimiendo cada elemento del arreglo
     cout << act[i] -> to_string();
 }
 void Club::muestra_activos(string tipo_activo) {
-
-  //Ciclo que va imprimiendo cada elemento del arreglo, con la condicion de que el tipo de activo sea el que le especificamos
-  for (int i = 0; i < infraestructura; i++) {
+  for (int i = 0; i < infraestructura; i++) { //Ciclo que va imprimiendo los elementos del arreglo que son del tipo que le especificamos
     if (act[i] -> get_tipo_activo() == tipo_activo)
       cout << act[i] -> to_string();
   }
 }
 //se crea una funcion de agregar para cada tipo de empleado
 void Club::agrega_jugador(string nombre, double salario_mensual) {
+  //new crea el objeto en tiempo de ejecución para usar polimorfismo
   act[infraestructura] = new Jugador(infraestructura, nombre, salario_mensual);
   infraestructura++;
 }
@@ -60,17 +58,16 @@ void Club::agrega_ejecutivo(string nombre, double salario_mensual) {
   act[infraestructura] = new Ejecutivo(infraestructura, nombre, salario_mensual);
   infraestructura++;
 }
+//funciones agrega para los otros tipos de activos (no empleado)
 void Club::agrega_inmueble(string nombre, string localizacion, double costo_mensual_mantenimiento) {
-
   act[infraestructura] = new Inmueble(infraestructura, nombre, localizacion, costo_mensual_mantenimiento);
   infraestructura++;
 }
 void Club::agrega_patrocinio(string nombre, double valor_anual) {
-
-  //new crea el objeto en tiempo de ejecución para usar polimorfismo
   act[infraestructura] = new Patrocinio(infraestructura, nombre, valor_anual);
   infraestructura++;
 }
+
 double Club::calcula_balance_anual() {
   double balance = 0;                //se va a ir acumulando la sumatoria a traves del ciclo for en esta variable
   Empleado *ep = NULL;
@@ -79,17 +76,17 @@ double Club::calcula_balance_anual() {
   for (int i = 0; i < infraestructura; i++) {
     if (act[i] -> get_tipo_activo() == "Empleado") {       //creamos un caso para cada clase hija de la clase activo
       ep = (Empleado*) act[i];                            //volvemos el apuntador de la clase padre a un apuntador de la clase hija
-      balance -= ep -> pago_anual_con_bonus();
+      balance -= ep -> pago_anual_con_bonus(); //al valor que en ese momento tiene balance, le resta el pago anual total del empleado
     }
     else if (act[i] -> get_tipo_activo() == "Inmueble"){
       ip = (Inmueble*) act[i];
-      balance -= ip -> get_costo_mensual_mantenimiento()*12;
+      balance -= ip -> get_costo_mensual_mantenimiento()*12; //al valor que en ese momento tiene balance le resta el pago mensual multiplicado por 12 del mantenimiento
     }
     else if (act[i] -> get_tipo_activo() == "Patrocinio"){
       pp = (Patrocinio*) act[i];
-      balance += pp -> get_valor_anual();
+      balance += pp -> get_valor_anual(); //al valor que en ese momento tiene balance le suma el valor del patrocinio, es decir, el dinero que otorgan las marcas al club
     }
   }
-return balance;
+return balance; //regresa el valor final de balance, el cual se guarda en caso de agregar mas activos posteriormente al calculo original de balance 
 }
 #endif
